5b3afe110b30f239d2549a84ff15a84085adee1a
alpha <wolfgang.schuessel@gmail.com>
2013-06-10 13:35:46 +0200

    wp

--- spec/r20130605_spec.rb
+++ spec/r20130605_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
-
-
 describe R20130605 do
-
 
   it 'should return correct version string' do
     R20130605.version_string.should == "R20130605 version #{R20130605::VERSION}"
@@ -16,11 +13,11 @@ describe R20130605 do
 
   it "should increment the count by 2" do
     expect do
-      expect{R20130605::RCounter.increment}.to change{R20130605::RCounter.count}.by(2)
+      expect { R20130605::RCounter.increment }.to change { R20130605::RCounter.count }.by(2)
     end.to raise_error
 
     expect do
-      expect{R20130605::RCounter.increment}.to change{R20130605::RCounter.count}.by(1)
+      expect { R20130605::RCounter.increment }.to change { R20130605::RCounter.count }.by(1)
 
     end.to_not raise_exception
   end
